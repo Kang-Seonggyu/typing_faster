f3d602db324a9572522077807dbe4ba5ac3d7f15
Dev_kang <jokekim609@gmail.com>
2024-02-13 15:55:08 +0900

    feat : 마지막 엔터시 속도 측정

--- src/components/OneSpelling.vue
+++ src/components/OneSpelling.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="spelling">
-    <span>{{ $props.spell }}</span>
-    <span>{{ $props.typing }}</span>
+    <span :class="props.typing && props.typing !== props.spell && 'incorrect'">
+      {{ props.spell }}
+    </span>
   </div>
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   spell: { type: String, required: true },
   typing: { type: String },
 });
@@ -15,10 +16,15 @@ defineProps({
 <style scoped>
 .spelling {
   display: inline-block;
-  word-spacing: 10px;
   white-space: pre;
 }
 span {
   color: #999;
+}
+span.incorrect {
+  position: relative;
+  z-index: 10;
+  color: rgba(255, 0, 0, 0.7);
+  font-weight: bold;
 }
 </style>
--- src/components/TypingBox.vue
+++ src/components/TypingBox.vue
@@ -4,32 +4,91 @@
     <div
       class="typing__container"
       @input="onTyping"
-      ref="editable"
+      @keydown="onKeydown"
+      ref="editableDiv"
       contenteditable="true"
     ></div>
     <div class="dictation__label">
       <OneSpelling
-        v-for="(spell, idx) in msg"
+        v-for="(spell, idx) in msg.replace(/&nbsp;/g, ' ')"
         :key="`spell_${idx}`"
         :spell="spell"
+        :typing="typingText[idx]"
       />
+    </div>
+    <div class="speed">
+      <em style="color: rgb(118, 103, 238)">WPM</em>
+      <p>{{ calculateTypingSpeed(startTime, endTime)[0] || '0' }}</p>
+      <em style="color: rgb(0, 211, 175)">CPM</em>
+      <p>{{ calculateTypingSpeed(startTime, endTime)[1] || '0' }}</p>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted, computed } from 'vue';
 import OneSpelling from './OneSpelling.vue';
 
 const props = defineProps({
   msg: { type: String, required: true },
 });
 
-const editable = ref(null);
+const editableDiv = ref(null);
+const startTime = ref(0);
+const endTime = ref(0);
 const typingText = ref('');
+const errorMsg = ref('');
+const typingCount = ref(0);
+
 const onTyping = (e) => {
-  typingText.value = e.target.innerHTML;
+  if (!startTime.value) {
+    startTime.value = Date.now();
+  }
+  typingText.value = e.target.innerHTML.replace(/&nbsp;/g, ' ');
   // moveCaret();
+};
+
+const onKeydown = (e) => {
+  const keyCode = e.keyCode || e.which;
+
+  if (e.code === 'Backspace') {
+    if (typingCount.value === 0) {
+      typingCount.value = 0;
+    } else {
+      typingCount.value -= 1;
+    }
+  } else if (e.code.includes('Enter')) {
+    e.preventDefault();
+    if (typingText.value === props.msg) {
+      errorMsg.value = '';
+      endTime.value = Date.now();
+    } else {
+      errorMsg.value = '오타가 있습니다. 확인해주세요.';
+    }
+  } else if (
+    (keyCode >= 65 && keyCode <= 90) || // A-Z
+    (keyCode >= 97 && keyCode <= 122) || // a-z
+    (keyCode >= 48 && keyCode <= 57) || // 0-9
+    keyCode === 188 || // ',' : comma
+    keyCode === 190 || // '.' : period
+    keyCode === 229
+  ) {
+    typingCount.value += 1;
+  } else {
+    return;
+  }
+};
+
+const calculateTypingSpeed = (start, end) => {
+  if (start === 0 && end === 0) {
+    return 0;
+  }
+  const elapsedSeconds = (end - start) / 1000;
+  const typingwordSpeed =
+    Math.round((typingText.value.length / elapsedSeconds) * 16) || 0;
+  const typingSpeed =
+    Math.round((typingCount.value / elapsedSeconds) * 60) || 0;
+  return [typingwordSpeed, typingSpeed];
 };
 
 // const caret = ref(null);
@@ -47,11 +106,11 @@ const onTyping = (e) => {
 // };
 
 onMounted(() => {
-  editable.value.focus();
+  editableDiv.value.focus();
   // moveCaret();
 });
 onUnmounted(() => {
-  editable.value.removeEventListener('input', handleInput);
+  editableDiv.value.removeEventListener('input', handleInput);
 });
 </script>
 
@@ -61,14 +120,15 @@ onUnmounted(() => {
   overflow: hidden;
   box-sizing: border-box;
   width: 100%;
-  padding: 20px 60px;
+  min-height: 200px;
+  padding: 20px 60px 20px 100px;
 }
 .typing__container {
   position: absolute;
   box-sizing: border-box;
   word-break: break-all;
   top: 20px;
-  left: 60px;
+  left: 100px;
   right: 60px;
   bottom: 20px;
   z-index: 10;
@@ -88,6 +148,24 @@ onUnmounted(() => {
   width: 2px;
   background-color: rgb(254, 192, 0);
   animation: blink-effect infinite step-end 1s;
+}
+.speed {
+  position: absolute;
+  left: 40px;
+  top: 20px;
+  padding: 4px;
+  text-align: center;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+}
+.speed em {
+  font-style: normal;
+
+  font-size: 14px;
+  font-weight: bold;
+}
+.speed p {
+  margin: 0;
 }
 /* .dictation__label {
 } */
